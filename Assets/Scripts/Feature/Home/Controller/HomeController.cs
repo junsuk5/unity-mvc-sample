ff56b657825b405cb53c39e4dcc9329ff01a5a23
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-09-19 08:46:39 +0900

    refactor(home) : 코드 개선

--- Assets/Scripts/Feature/Home/Controller/HomeController.cs
+++ Assets/Scripts/Feature/Home/Controller/HomeController.cs
@@ -8,30 +8,22 @@ namespace Feature.Home.Controller
 {
     public class HomeController : MonoBehaviour, IMonoEventListener
     {
-        void Start()
-        {
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        }
-
         public EventChain OnEventHandle(IEvent param)
         {
-            if (param is OnClickStartGameEvent)
+            switch (param)
             {
-                Debug.Log("OnClickStartGameEvent");
-                SceneManager.LoadScene(RouteNames.Play);
-                // Addressables.LoadSceneAsync(RouteNames.Play);
-            }
-            else if (param is OnClickImageSearchEvent)
-            {
-                Debug.Log("OnClickImageSearchEvent");
-                SceneManager.LoadScene(RouteNames.Search);
+                case OnClickStartGameEvent:
+                    Debug.Log("OnClickStartGameEvent");
+                    SceneManager.LoadScene(RouteNames.Play);
+                    return EventChain.Break;
+
+                case OnClickImageSearchEvent:
+                    Debug.Log("OnClickImageSearchEvent");
+                    SceneManager.LoadScene(RouteNames.Search);
+                    return EventChain.Break;
             }
 
-            return EventChain.Break;
+            return EventChain.Continue;
         }
     }
 }
